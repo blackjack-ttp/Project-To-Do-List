fd2268a37c7e9279352cc338a70e4387c4914068
blackjack-ttp <phongtt120298@outlook.com>
2023-09-28 20:39:13 +0700

    #2 [Feature] Update function: add todo and save localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
 import { AiOutlineDelete, AiFillCheckSquare } from 'react-icons/ai';
 
 function App() {
   const [isCompleted, setIsCompleted] = useState(false);
+  const [allTodo, setTodos] = useState([]);
+  const [newTitle, setNewTitle] = useState('');
+  const [newDescription, setNewDescription] = useState('');
+
+  const handleChangeTitle = (e) => {
+    setNewTitle(e.target.value);
+  };
+
+  const handleChangeDescription = (e) => {
+    setNewDescription(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    let newTodoItem = {
+      title: newTitle,
+      description: newDescription,
+    };
+
+    let updateTodoArr = [...allTodo];
+
+    updateTodoArr.push(newTodoItem);
+    setTodos(updateTodoArr);
+    localStorage.setItem('todolist', JSON.stringify(updateTodoArr));
+    setNewTitle('');
+    setNewDescription('');
+  };
+
+  useEffect(() => {
+    let savedTodo = JSON.parse(localStorage.getItem('todolist'));
+    if (savedTodo) {
+      setTodos(savedTodo);
+    }
+  }, []);
+
   return (
     <>
       <div className="App">
@@ -12,14 +46,24 @@ function App() {
           <div className="todo__wrapper__input">
             <div className="todo__wrapper__input__item">
               <label>Title</label>
-              <input type="text" placeholder="What's the task title?" />
+              <input
+                type="text"
+                value={newTitle}
+                onChange={handleChangeTitle}
+                placeholder="What's the task title?"
+              />
             </div>
             <div className="todo__wrapper__input__item">
               <label>Description</label>
-              <input type="text" placeholder="What's the task description?" />
+              <input
+                type="text"
+                value={newDescription}
+                onChange={handleChangeDescription}
+                placeholder="What's the task description?"
+              />
             </div>
             <div className="todo__wrapper__input__item">
-              <button type="button" className="primaryBtn">
+              <button type="button" onClick={handleAddTodo} className="primaryBtn">
                 Add
               </button>
             </div>
@@ -38,20 +82,27 @@ function App() {
               Completed
             </button>
           </div>
-          <div className="todo-list">
-            <div className="todo-list__item">
-              <h3>Task 1</h3>
-              <p>Description</p>
-            </div>
-            <div className="todo-list__icon">
-              <div className="todo-list__icon__delete">
-                <AiOutlineDelete />
-              </div>
-              <div className="todo-list__icon__check">
-                <AiFillCheckSquare />
-              </div>
-            </div>
-          </div>
+
+          {allTodo.map((item, index) => {
+            return (
+              <>
+                <div className="todo-list">
+                  <div className="todo-list__item" key={index}>
+                    <h3>{item.title}</h3>
+                    <p>{item.description}</p>
+                  </div>
+                  <div className="todo-list__icon">
+                    <div className="todo-list__icon__delete">
+                      <AiOutlineDelete />
+                    </div>
+                    <div className="todo-list__icon__check">
+                      <AiFillCheckSquare />
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
         </div>
       </div>
     </>
